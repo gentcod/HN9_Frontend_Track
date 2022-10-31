d96079b1f487f95ae528a2445df988dca2a5f73d
Oyefule <drelanorgent@gmail.com>
2022-10-31 03:25:29 +0100

    Changed LinkContainer background color and hover effect

--- src/components/link-box/link-box.styles.jsx
+++ src/components/link-box/link-box.styles.jsx
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
 
+export const LinkBoxText = styled.p`
+   text-align: center;
+   font-weight: 700;
+`
+
 export const LinkBoxContainer = styled.div`
    width: 100%;
-   background-color: #eee;
+   background-color: #EAECF0;
    border-radius: .5rem;
    margin-bottom: 1.5rem;
    padding: 1.5rem;
@@ -10,14 +15,13 @@ export const LinkBoxContainer = styled.div`
    &:hover {
       background-color: #d8d8d8;
    }
+
+   &:hover ${LinkBoxText} {
+      color: #777;
+   }
 `
 
 export const LinkBoxTextContainer = styled.a`
    height: 100%;
    width: 100%;
-`
-
-export const LinkBoxText = styled.p`
-   text-align: center;
-   font-weight: 700;
 `
